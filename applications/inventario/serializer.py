c6f53d5b79fd4af4c0c1eb4f3abaa4592a86d942
miguel.rodriguez <miguel.rodriguez@acceso.com.pe>
2022-01-31 20:56:27 -0500

    add id_invoice serializer inventario invoice

--- applications/inventario/serializer.py
+++ applications/inventario/serializer.py
@@ -1,20 +1,36 @@
 from rest_framework import serializers
 from .models import ProductModel, ProveedoresModel, ServiceModel, FacturaModel
 
+
 class ProveeSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProveedoresModel
         fields = '__all__'
+
 
 class ServiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = ServiceModel
         fields = '__all__'
 
+
 class InvoiceSerializer(serializers.ModelSerializer):
+    id_invoice = serializers.SerializerMethodField()
+
     class Meta:
         model = FacturaModel
-        fields = '__all__'
+        fields = (
+            'id',
+            'created',
+            'modified',
+            'type_invoice',
+            'codigo',
+            'proveedor',
+            'id_invoice'
+        )
+
+    def get_id_invoice(self, obj):
+        return obj.id
 
 
 class ProductoSerializer(serializers.ModelSerializer):
